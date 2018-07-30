8cb99cf18169a61d1acdabf187a0e768de06c972
glyphack <sh.hooshyari@gmail.com>
2018-07-30 14:18:08 +0430

    created url to show all orders

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    url(r'^create/$', views.order_create, name='order_create')
+    url(r'^create/$', views.order_create, name='order_create'),
+    url(r'^allorders/$', views.OrderListView.as_view(), name='order_list')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from users.decorators import customer_required
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
 
 @login_required
 @customer_required
@@ -11,7 +12,6 @@ def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
-        # form.fields['address'] = request.user.customer.address
         if form.is_valid():
             order = form.save()
             for item in cart:
@@ -24,5 +24,12 @@ def order_create(request):
             cart.clear()
         return render(request, 'order/created.html', {'order': order})
     else:
-        form = OrderCreateForm(request = request)
+        form = OrderCreateForm()
     return render(request, 'order/create.html', {'form': form})
+
+class OrderListView(ListView):
+    template_name = 'order/all_orders.html'
+    context_object_name = 'orders_list'
+    
+    def get_queryset(self):
+        return Order.objects.order_by('-created')[:]
